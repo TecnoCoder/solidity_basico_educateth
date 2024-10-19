710c3a32501d75e4cd86928f9d885b656fe94611
TecnoCoder <Gianfranco325@gmail.com>
2024-10-18 20:44:10 -0400

    primer contrato

---
+++ hola_mundo.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;  // en esta parte defino la version del compilador
+
+// a partir de acá coloco la logica de mi contrato
+contract hola_mundo {
+
+    string public saludo = "Hola desde Educateth"; // declarando el contenido de mi variable saludo
+    
+}
